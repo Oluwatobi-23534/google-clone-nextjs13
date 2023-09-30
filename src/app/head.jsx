73c6f81183a4af9f5cba77629ab4ad525fb458b6
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-09-30 23:58:38 +0100

    install tailwind css

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -4,7 +4,7 @@ export default function Head() {
         <title>Google Clone Nextjs 13</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="description" content="Google clone created by Nextjs 13" />
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.svg"/>
     </>
   )
 }
