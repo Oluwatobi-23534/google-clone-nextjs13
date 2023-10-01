d7e3df3c9112f5b99303ad3a9be1d24bf0ed46b1
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-10-01 01:01:33 +0100

    add home header component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,11 @@
 import './globals.css'
+import Head from './head';
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head/>
       <body className="relative min-h-screen">{children}</body>
     </html>
   );
